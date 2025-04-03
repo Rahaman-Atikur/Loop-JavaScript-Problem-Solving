3ff020e49c2dde1b77a6d536d1ca6d296ecf50fc
Atikur Rahaman <atikur.rahaman.hs@gmail.com>
2025-04-03 21:37:30 +0100

    introduction to for done

--- for.js
+++ for.js
@@ -2,3 +2,8 @@ const numbers = [1,2,3,4,6,7,8];
 for(const num of numbers){
     console.log(num);
 }
+
+const fruits =['Banana','Orange','Mango'];
+for(const fruit of fruits){
+    console.log(fruit);
+}
