f3505c03000dbb53a7a023714cfe72fc17db1ea0
Atikur Rahaman <atikur.rahaman.hs@gmail.com>
2025-04-04 18:59:50 +0100

    table done

---
+++ table.js
@@ -0,0 +1,19 @@
+/***
+
+As Ersa is learning now, she wants to explore more and more. Tell Ersa to generate a multiplication table for number 5
+
+ */
+
+
+/*programming hero*/
+// 5* 1 = 5
+// 5 * 2 = 10
+
+let number = 5;
+let i = 1;
+let mul;
+while(i<=10){
+    mul = number*i;
+    console.log(number +'*' +i +'=' +mul);
+    i++;
+}
