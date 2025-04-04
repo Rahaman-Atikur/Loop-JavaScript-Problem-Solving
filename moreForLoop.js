f1ddb89627d1f1181a715aef7ca630a8d84d21e3
Atikur Rahaman <atikur.rahaman.hs@gmail.com>
2025-04-04 08:23:28 +0100

    commitment done

---
+++ moreForLoop.js
@@ -0,0 +1,12 @@
+let sum = 0;
+for (let i = 11; i <= 20; i++) {
+    sum = sum + i;
+    console.log(i);
+}
+console.log('sum of 11 to 20 is:', sum);
+
+// Decremental Loop
+
+for(let i =10;i>=0;i--){
+    console.log(i);
+}
